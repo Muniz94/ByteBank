27a0af2df4eab99d7bb970d12e963ae11bcd4cd9
Lucas Muniz <muniz@hotmail.com>
2023-11-23 08:21:44 -0300

    Ao 'submit' do form a página é 'prevenida' de atualizar

--- js/bytebank.js
+++ js/bytebank.js
@@ -3,3 +3,7 @@ let saldo = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 elementoSaldo.textContent = saldo;
 
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener ("submit", function(event) {
+  event.preventDefault();
+})
