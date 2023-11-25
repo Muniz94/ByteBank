d5c399d29434e0169c456d158a31dcd8bf9c55fa
Lucas Muniz <muniz@hotmail.com>
2023-11-25 18:33:35 -0300

    Alterei o 'typescript.ts'

--- src/typescript.ts
+++ src/typescript.ts
@@ -4,3 +4,21 @@ let nome: string = "";
 let isPago: boolean = false;
 let qualquer: any = "";
 qualquer = 22;
+
+// Arrays
+const lista = []; // pode receber qualquer coisa
+const num: number[] = []; // apenas números
+
+// Tipos Personalizados (Type Alias)
+type Transacao = {             
+  tipoTransacao: string;     // Esse bloco define uma estrutura padrão para todo objeto do tipo 'Transacao'
+  data: Date;
+  valor: number;
+}
+
+const novaTransacao: Transacao = {
+  tipoTransacao: "Lucas",
+  data: new Date(),
+  valor: 10
+}
+
