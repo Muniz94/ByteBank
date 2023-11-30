28fddc3f294726b4a1343402a0d753e2d4c34aab
Lucas Muniz <muniz@hotmail.com>
2023-11-30 16:08:37 -0300

    Criei a função 'formatarInformações'

--- src/FormatoData.ts
+++ src/FormatoData.ts
@@ -3,3 +3,9 @@ enum FormatoData {
   DIA_SEMANA_DIA_MES_ANO = "DIA_SEMANA, DD/MM/AAAA",
   DIA_MES = "DD/MM"
 }
+
+function formatarInformacoes(valor: number, data: Date, formatoData: FormatoData): string {
+  const dataFormatada = formatarData(data, formatoData);
+  const valorFormatado = formatarMoeda(valor);
+  return `${dataFormatada} - ${valorFormatado}`;
+}
