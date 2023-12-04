ece7a959363b9ef2a86253829efc0c36a5ad2877
Lucas Muniz <muniz@hotmail.com>
2023-12-04 19:32:38 -0300

    Importei o 'data-component.js' no 'main.ts'

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 // Esse arquivo pode ser chamado de 'UP.ts' também
 import "./components/nova-transacao-component.js";
 import "./components/saldo-component.js";
+import "./components/data-component.js";
