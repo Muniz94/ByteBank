c9d77b7beddcb3df205f2df86ea52822f9afc286
muniz94 <muniz91@hotmail.com>
2023-12-04 19:28:13 -0300

    Refatorei o código para organização

--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -1,25 +1,18 @@
-import { formatarData, formatarMoeda } from "../utils/formatters.js";
-import { FormatoData } from "../types/FormatoData.js";
-
-let saldo = 3000;
+import { formatarMoeda } from "../utils/formatters";
+import Conta from "../types/Conta";
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
-const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
-if (elementoDataAcesso){ // diferente de null
-  const dataAcesso: Date = new Date();
-  elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+function renderizarSaldo(): void {
+    if (elementoSaldo !== null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+    }
 }
 
-export function getSaldo(): number {
-  return saldo;
-}
+const SaldoComponente = {
+    atualizar: function () {
+        renderizarSaldo();
+    },
+};
 
-atualizarSaldo(saldo);
-
-export function atualizarSaldo(novoSaldo: number): void {
-  saldo = novoSaldo;
-  if (elementoSaldo){ // diferente de null
-    elementoSaldo.textContent = formatarMoeda(saldo)
-  }
-}
+export default SaldoComponente;
