973604c46bb8c3430fa16c260e8750396c46e80d
Lucas Muniz <muniz@hotmail.com>
2023-12-05 19:50:02 -0300

    Com o try/catch foi possível exibir as msg de erro em um alert ao invés do console

--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -5,27 +5,29 @@ import Conta from "../types/Conta.js";
 
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 elementoFormulario.addEventListener ("submit", function(event) {
-  event.preventDefault();
-  if (!elementoFormulario.checkValidity()) {
-    alert("Por favor, preencha todos os campos da transação!");
-    return;
+  try
+  {
+      event.preventDefault();
+      if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos da transação!");
+        return;
+      }
+      const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao') as HTMLSelectElement;
+      const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
+      const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
+      let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
+      let valor: number = inputValor.valueAsNumber; // os inputs.value são strings por isso deve-se converter
+      let data: Date = new Date(inputData.value);
+      const novaTransacao: Transacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data,
+      }
+      Conta.registrarTransacao(novaTransacao);
+      SaldoComponente.atualizar();
+      elementoFormulario.reset();
   }
-
-  const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao') as HTMLSelectElement;
-  const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
-  const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
-
-  let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
-  let valor: number = inputValor.valueAsNumber; // os inputs.value são strings por isso deve-se converter
-  let data: Date = new Date(inputData.value);
-
-  const novaTransacao: Transacao = {
-    tipoTransacao: tipoTransacao,
-    valor: valor,
-    data: data,
+  catch(erro) {
+    alert(erro.message);
   }
-
-  Conta.registrarTransacao(novaTransacao);
-  SaldoComponente.atualizar();
-  elementoFormulario.reset();
 })
