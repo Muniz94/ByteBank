13110d4ca6a98a1ed571e731717580302d957a82
muniz94 <muniz91@hotmail.com>
2023-12-05 20:54:41 -0300

    Adicionei todo o projeto para teste (está com erro)

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,10 +1,14 @@
-let saldo = 3000;
+import { formatarMoeda } from "../utils/formatters.js";
+import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
-const elementoDataAcesso = document.querySelector(".block-saldo time");
-if (elementoSaldo) { // diferente de null
-    elementoSaldo.textContent = formatarMoeda(saldo);
+function renderizarSaldo() {
+    if (elementoSaldo !== null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+    }
 }
-if (elementoDataAcesso) { // diferente de null
-    const dataAcesso = new Date();
-    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
-}
+const SaldoComponente = {
+    atualizar: function () {
+        renderizarSaldo();
+    },
+};
+export default SaldoComponente;
--- dist/js/types/FormatoData.js
+++ dist/js/types/FormatoData.js
@@ -1,11 +1,6 @@
-var FormatoData;
+export var FormatoData;
 (function (FormatoData) {
     FormatoData["PADRAO"] = "DD/MM/AAAA";
     FormatoData["DIA_SEMANA_DIA_MES_ANO"] = "DIA_SEMANA, DD/MM/AAAA";
     FormatoData["DIA_MES"] = "DD/MM";
 })(FormatoData || (FormatoData = {}));
-function formatarInformacoes(valor, data, formatoData) {
-    const dataFormatada = formatarData(data, formatoData);
-    const valorFormatado = formatarMoeda(valor);
-    return `${dataFormatada} - ${valorFormatado}`;
-}
