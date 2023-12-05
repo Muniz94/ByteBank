919016e79efacdcdd7cbf4f89bd50fb6509c6c8d
muniz94 <muniz91@hotmail.com>
2023-12-05 19:45:45 -0300

    Criei as funções 'debitar' e 'depositar'

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -3,6 +3,25 @@ import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 3000;
 
+function debitar(valor: number): void {
+  if (valor <= 0){
+    throw new Error("O valor a ser debitado deve ser maior que zero!");
+  }
+  if (valor > saldo){
+    throw new Error("Saldo insuficiente!");
+  }
+
+  saldo -= valor;
+}
+
+function depositar(valor: number): void {
+  if (valor <= 0){
+    throw new Error("O valor a ser depositado deve ser maior que zero!");
+  }
+
+  saldo += valor;
+}
+
 const Conta = {
   getSaldo() {
     return saldo;
