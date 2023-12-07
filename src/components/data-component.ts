b090870643fe38a8b38567a296f2fc89dcc154f0
muniz94 <muniz91@hotmail.com>
2023-12-07 20:34:46 -0300

    As funções de renderizar estavam sendo criadas mas não chamadas

--- src/components/data-component.ts
+++ src/components/data-component.ts
@@ -4,6 +4,7 @@ import Conta from "../types/Conta.js";
 
 const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
+renderizarData();
 function renderizarData(): void {
     if (elementoDataAcesso !== null) {
         elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
