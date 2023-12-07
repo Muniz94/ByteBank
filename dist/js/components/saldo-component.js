973981b18fec4e7020b5bbd20e092de897d61add
Lucas Muniz <muniz@hotmail.com>
2023-12-07 20:34:46 -0300

    As funções de renderizar estavam sendo criadas mas não chamadas

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,6 +1,7 @@
 import { formatarMoeda } from "../utils/formatters.js";
 import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+renderizarSaldo();
 function renderizarSaldo() {
     if (elementoSaldo !== null) {
         elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -3,6 +3,7 @@ import Conta from "../types/Conta.js";
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 
+renderizarSaldo();
 function renderizarSaldo(): void {
     if (elementoSaldo !== null) {
         elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
