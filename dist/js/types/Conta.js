daf6a93d9557c2ce264cbd6bdb48ecfbcb5c1ff4
Lucas Muniz <muniz@hotmail.com>
2023-12-30 09:44:46 -0300

    Dei um return no 'GrupoTransações'

--- dist/js/types/Conta.js
+++ dist/js/types/Conta.js
@@ -30,6 +30,24 @@ const Conta = {
     getDataAcesso() {
         return new Date();
     },
+    getGruposTransacoes() {
+        const gruposTransacoes = [];
+        const listaTransacoes = structuredClone(transacoes); // structuredClone literalmente cria um clone (transações no caso)
+        const transacoesOrdenadas = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime()); // t2 - t1 (decrescente) //// t1 - t2 (crescente)
+        let labelAtualGrupoTransacao = ""; // nome do Grupo de transações
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao = transacao.data.toLocaleDateString('pt-br', { month: 'long', year: 'numeric' });
+            if (labelAtualGrupoTransacao != labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoTransacao,
+                    transacoes: [],
+                });
+            }
+            gruposTransacoes.at(-1).transacoes.push(transacao);
+        }
+        return gruposTransacoes;
+    },
     registrarTransacao(novaTransacao) {
         if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
             depositar(novaTransacao.valor);
